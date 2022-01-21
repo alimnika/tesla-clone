d89a88e63b05593f41dfd0459719225683ff549a
alimnika <alimnika87@gmail.com>
2022-01-21 15:47:38 -0600

    sidebar Menu is done

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,9 @@
-import React from "react";
+import React, { useState }from "react";
 import styled from "styled-components";
-import MenuIcon from '@mui/icons-material/Menu';
-
-
+import MenuIcon from "@mui/icons-material/Menu";
+import CloseIcon from '@mui/icons-material/Close';
 function Header() {
+  const [open, setOpen] = useState(false)
   return (
     <Container>
       <a href="#">
@@ -21,31 +21,75 @@ function Header() {
       <RightMenu>
         <a href="#">Shop</a>
         <a href="#">Tesla Account</a>
-        <CustomMenu />
+        <CustomMenu onClick={()=>setOpen(true)} />
       </RightMenu>
+      <BurgerMenu show={open}>
+        <CloseWrapper >
+        <CustomClose  onClick={()=>setOpen(false)} />
+        </CloseWrapper>
+        <li>
+          <a href="">Existing Inventory</a>
+        </li>
+        <li>
+          <a href="">Used Inventory</a>
+        </li>
+        <li>
+          <a href="">Trade-In</a>
+        </li>
+        <li>
+          <a href="">Test Drive</a>
+        </li>
+        <li>
+          <a href="">CyberTruck</a>
+        </li>
+        <li>
+          <a href="">Semi</a>
+        </li>
+        <li>
+          <a href="">Charging</a>
+        </li>
+        <li>
+          <a href="">Powerwall</a>
+        </li>
+        <li>
+          <a href="">Commercial Energy</a>
+        </li>
+        <li>
+          <a href="">Utilities</a>
+        </li>
+        <li>
+          <a href="">Find us</a>
+        </li>
+        <li>
+          <a href="">Support</a>
+        </li>
+        <li>
+          <a href="">Investor Relations</a>
+        </li>
+      </BurgerMenu>
     </Container>
   );
 }
 export default Header;
 
 const Container = styled.div`
+  z-index: 1;
   min-height: 60px;
   position: fixed;
   display: flex;
-  align-items:center;
+  align-items: center;
   justify-content: space-between;
   top: 0;
   left: 0;
   right: 0;
 
-  img{
+  img {
     height: 80px;
   }
- 
 `;
 // const Image = styled.img`
 //   // width:50%;
-  
+
 // `;
 const MenuCenter = styled.div`
   display: flex;
@@ -59,8 +103,8 @@ const MenuCenter = styled.div`
     padding: 0 10px;
     flex-wrap: no-wrap;
   }
-  @media(max-width:768px){
-    display:none;
+  @media (max-width: 768px) {
+    display: none;
   }
 `;
 const RightMenu = styled.div`
@@ -70,9 +114,40 @@ const RightMenu = styled.div`
   a {
     font-weight: 600;
     text-transform: uppercase;
-    margin-right:10px;
+    margin-right: 10px;
   }
 `;
 const CustomMenu = styled(MenuIcon)`
-cursor:pointer;
+  cursor: pointer;
+`;
+const BurgerMenu = styled.div`
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  right: 0;
+  background: white;
+  width: 300px;
+  z-index: 16;
+  list-style:none;
+  padding: 20px;
+  display:flex;
+  flex-direction: column;
+  text-align:start;
+  transform: ${props=>props.show ? 'translateX(0)': 'translateX(100%)'};
+  transition: transform 0.1s ease-in;
+  li{
+    padding: 15px 0;
+    border-bottom: 1px solid rgba(0,0,0,0.2);
+    a{
+      font-weight: 600;
+    }
+  }
+  
+`;
+const CloseWrapper = styled.div`
+  display:flex;
+  justify-content: flex-end;
+`
+const CustomClose = styled(CloseIcon)`
+cursor: pointer;
 `
